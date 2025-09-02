92d9d412655ea3f7cfbed4beb910412a8432ab78
archirajpoot <archirajpoot21@gmail.com>
2025-09-02 13:21:58 +0530

    loop4

---
+++ loops/for3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+   
+   for(int i=0;i<=10;i++)
+   {
+     
+    if(i%2==0){
+        printf("%d",i);
+        printf("even number\n");
+       
+    }
+      
+    else{
+         printf("%d",i);
+        printf("odd number\n");
+        
+    }
+   }
+   return 0;
+
+}
