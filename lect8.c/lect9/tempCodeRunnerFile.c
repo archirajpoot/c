80e5d013db729fe37b75ffac0c9957d338c9466b
archirajpoot <archirajpoot21@gmail.com>
2025-09-09 13:00:26 +0530

    lect9

---
+++ lect8.c/lect9/tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+int main()
+// { 
+//     char ch='A';
+//     for(int i=1;i<4;i++)
+//     {
+//         for(int j=1;j<=i;j++)
+//          {
+           
+//             printf("%c",ch);
+//             ch++;
+//          }
+//          printf("\n");
+//     }
+//     return 0;
+// }
