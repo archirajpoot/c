2a6ccce8675f5386c1d024cddbe5ed78eb5f7178
archirajpoot <archirajpoot21@gmail.com>
2025-09-15 12:33:03 +0530

    lect11

---
+++ lect11/funtion1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void odd_even(int a){
+   if(a%2==0){
+    printf("even");
+   }else{
+    printf("oddd");
+   }
+printf("hello");
+return 0;
+
+}
+// int sum(){
+//     int a=5;
+//     int b=5;
+//     printf("%d",a+b);
+// }
+int main(){
+     int a;
+     scanf("%d",&a);
+    odd_even(a);
+    odd_even(a);
+    // odd_even();
+    // odd_even();
+   
+    return 0;
+    //  printf("hello");
+
+
+}
+
+
+   
